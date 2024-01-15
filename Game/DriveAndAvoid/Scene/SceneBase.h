433494bb6c46b95f6dc4775fc1bb71b6bf609c2d
haruto68 <138412489+haruto68@users.noreply.github.com>
2024-01-15 12:25:54 +0900

    1/15
    Scene
      ~.h

--- Game/DriveAndAvoid/Scene/SceneBase.h
+++ Game/DriveAndAvoid/Scene/SceneBase.h
@@ -1,1 +1,31 @@
 #pragma once
+
+//シーン情報
+enum eSceneType
+{
+	E_TITLE,
+	E_MAIN,
+	E_HELP,
+	E_RANKING_DISP,
+	E_RANKING_INPUT,
+	E_RESULT,
+	E_END
+};
+
+//基礎シーンクラス
+class SceneBase
+{
+public:
+	SceneBase() {}
+	virtual ~SceneBase() {}
+
+	virtual void Initialize() {}	//初期化処理
+	virtual eSceneType Update()		//更新処理
+	{
+		return GetNowScene();
+	}
+	virtual void Draw() const {}	//描画処理
+	virtual void Finalize() {}		//終了時処理
+
+	virtual eSceneType GetNowScene() const = 0;	//現在のシーン情報を取得
+};
--- Game/DriveAndAvoid/Scene/SceneManager.h
+++ Game/DriveAndAvoid/Scene/SceneManager.h
@@ -1,1 +1,27 @@
 #pragma once
+
+#include"SceneBase.h"
+
+#define TARGET_FREAM_RATE	(60)	//固定化するフレームレート値
+#define DELTA_SECOND	(1000000 / TARGET_FREAM_RATE)
+									//1フレーム当たりの時間（マイクロ秒）
+
+//シーンマネージャークラス
+class SceneManager
+{
+private:
+	SceneBase* current_scene;	//現在シーン情報
+	
+public:
+	SceneManager();
+	~SceneManager();
+
+	void Initialize();	//初期化処理
+	void Update();		//更新処理
+	void Finalize();	//終了時処理
+
+private:
+	void Draw() const;								//描画処理
+	void ChangeScene(eSceneType scene_type);		//シーン切り替え処理
+	SceneBase* CreatScene(eSceneType scene_type);	//シーン生成処理
+};
